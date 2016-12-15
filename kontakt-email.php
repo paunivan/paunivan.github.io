e98c34cdd4b82fb440963d6ce290c939c3be031b
paunivan <paunivan@yahoo.com>
2016-12-15 18:11:17 +0100

    Update kontakt-email.php

--- kontakt-email.php
+++ kontakt-email.php
@@ -1,40 +1,12 @@
 <?php
-if(isset($_POST['submit'])) 
-{
-
-$message=
-'name:	'.$_POST['fullname'].'<br />
-subject:	'.$_POST['subject'].'<br />
-email:	'.$_POST['emailid'].'<br />
-message:	'.$_POST['message'].'
-';
-    require "phpmailer/class.phpmailer.php"; //include phpmailer class
-      
-    // Instantiate Class  
-    $mail = new PHPMailer();  
-      
-    // Set up SMTP  
-    $mail->IsSMTP();                // Sets up a SMTP connection  
-    $mail->SMTPAuth = true;         // Connection with the SMTP does require authorization    
-    $mail->SMTPSecure = "ssl";      // Connect using a TLS connection  
-    $mail->Host = "smtp.gmail.com";  //Gmail SMTP server address
-    $mail->Port = 465;  //Gmail SMTP port
-    $mail->Encoding = '7bit';
-    
-    // Authentication  
-    $mail->Username   = "paunivan86@gmail.com"; // Your full Gmail address
-    $mail->Password   = "@@temudzin@@"; // Your Gmail password
-      
-    // Compose
-    $mail->SetFrom($_POST['email'], $_POST['name']);
-    $mail->AddReplyTo($_POST['emailid'], $_POST['name']);
-    $mail->Subject = "New Contact Form Enquiry";      // Subject (which isn't required)  
-    $mail->MsgHTML($message);
- 
-    // Send To  
-    $mail->AddAddress("paunivan@yahoo.com", "paunivan"); // Where to send it - Recipient
-    $result = $mail->Send();		// Send!  
-	$message = $result ? 'Successfully Sent!' : 'Sending Failed!';      
-	unset($mail);
-
+$name = $_POST('name');
+$subject = $_POST('subject');
+$message = $_POST('message');
+$_message = "($name) Sends you a message \n ($message)";
+try{
+mail('paunivan86@gmail.com, $subject, $_message, "From: paunivan@yahoo.com");
+echo 'Mail sent';
+}catch (Exception $e){
+echo 'There is an error: '.$e->getMessage();
 }
+?>
